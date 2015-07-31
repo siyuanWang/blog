c92bd0ef1c627a59c09c4398e58a10bc5da6392f
王思远 <wsy2355883@163.com>
2015-07-31 14:20:51 +0800

    完善用户新增表单

--- app/scripts/controllers/article/add.js
+++ app/scripts/controllers/article/add.js
@@ -1,10 +1,34 @@
 var app = angular.module('addArticleApp', []);
-app.controller('addArticleFormController', function($scope) {
+app.controller('addArticleFormController', function($scope, $http) {
+
     $scope.submit = function() {
         if($scope.userForm.$valid) {
             alert("submit successfull.");
+            $http.post('', $scope.fields)
+                .success(function(data, status, headers, config) {
+                    alert('save success.')
+                })
+                .error(function(data, status, headers, config) {
+                    alert(data);
+                });
+        } else {
+
         }
-    }
+    };
+
+    $scope.master = {};
+    $scope.validateBeforSubmit = function(fields) {
+        $scope.master = angular.copy(fields);
+    };
+    $scope.reset = function(form) {
+        if (form) {
+            form.$setPristine();
+            form.$setUntouched();
+        }
+        $scope.fields = angular.copy($scope.master);
+    };
+
+    $scope.reset();
 });
 
 app.directive('equals', function() {
@@ -13,7 +37,6 @@ app.directive('equals', function() {
         require: '?ngModel', // get a hold of NgModelController
         link: function(scope, elem, attrs, ngModel) {
             if(!ngModel) return; // do nothing if no ng-model
-
             // watch own value and re-validate on change
             scope.$watch(attrs.ngModel, function() {
                 validate();
