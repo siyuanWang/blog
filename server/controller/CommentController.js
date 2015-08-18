85b1d7892cd8cc7e8c8bb0dc157cb84bf637fbd9
王思远 <wsy2355883@163.com>
2015-08-18 16:53:52 +0800

    add comment

--- server/controller/CommentController.js
+++ server/controller/CommentController.js
@@ -6,28 +6,28 @@ var router = express.Router();
  * 获得文章列表
  */
 router.get('/', function(req, res) {
-  console.log("#################get#############");
-  articleDao.query(function(result) {
-    res.send(result);
-  });
+    console.log("#################get#############");
+    articleDao.query(function(result) {
+        res.send(result);
+    });
 
 });
 /**
  * 根据_id获得对应的文章
  */
 router.get('/:id', function(req, res) {
-  articleDao.queryById(req.params.id, function(result) {
-    res.send(result);
-  });
+    articleDao.queryById(req.params.id, function(result) {
+        res.send(result);
+    });
 });
 /**
  * 新增一个文章
  */
 router.post('/', function(req, res) {
-  var data = req.body;
-  articleDao.save(data, function(data){
-    res.send(data.msg);
-  })
+    var data = req.body;
+    articleDao.save(data, function(data){
+        res.send(data.msg);
+    })
 });
 
 module.exports = router;
--- server/controller/UserController.js
+++ server/controller/UserController.js
@@ -1,3 +1,4 @@
+'use strict';
 var express = require('express');
 var userDao = require('../dao/user/UserDao');
 var router = express.Router();
