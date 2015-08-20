e189098faf58cdbc3487e498fc6c6497dcc6d77d
王思远 <wsy2355883@163.com>
2015-08-20 17:50:26 +0800

    update index

--- app/app.js
+++ app/app.js
@@ -6,7 +6,8 @@ define([], function() {
 
             function($routeProvider, routeResolverProvider, $controllerProvider, $compileProvider,
                      $provide, $filterProvider, $filter) {
-                console.log('app config.')
+                console.log('app config.');
+
                 app.register = {
                     controller: $controllerProvider.register,
                     directive: $compileProvider.directive,
@@ -20,13 +21,21 @@ define([], function() {
                     .when('/index',route.resolve('indexController','index','index', 'vm', false))
                     .when('/article/query/:id',route.resolve('articleShowController', 'show', 'article', 'vm', false))
                     .when('/article/add',route.resolve('articleAddController','add','article', 'vm', false))
-                    .otherwise(route.resolve('carouselController','carousel','carousel', 'vm', false));
+                    .otherwise(route.resolve('indexController','index','index', 'vm', false));
             }]
     );
     app.filter('sanitize',['$sce', function($sce) {
         return function(htmlCode){
             return $sce.trustAsHtml(htmlCode);
         }
+    }]);
+
+    app.run(['$rootScope','$location', '$routeParams', function($rootScope, $location, $routeParams) {
+        $rootScope.$on('$routeChangeSuccess', function(e, current, pre) {
+            console.log('Current route name: ' + $location.path());
+            var path = $location.path();//获得当前路由地址
+    
+        })
     }]);
 
     return app;
